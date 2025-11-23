22f1c2048aff27075cce90d5a535efd35fcb00cb
Alfred Neumayer <dev.beidl@gmail.com>
2025-11-23 04:44:01 +0100

    device & rootdir: Clean up unused and old files

--- device.mk
+++ device.mk
@@ -68,15 +68,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/system/halium/lib/udev/rules.d/70-android.rules:system/halium/lib/udev/rules.d/70-android.rules \
     $(DEVICE_PATH)/rootdir/system/halium/etc/ubuntu-touch-session.d/android.conf:system/halium/etc/ubuntu-touch-session.d/android.conf \
-    $(DEVICE_PATH)/rootdir/system/halium/etc/apparmor.d/local/usr.bin.media-hub-server:system/halium/etc/apparmor.d/local/usr.bin.media-hub-server \
     $(DEVICE_PATH)/rootdir/system/halium/etc/default/usb-moded.d/device-specific-config.conf:system/halium/etc/default/usb-moded.d/device-specific-config.conf \
     $(DEVICE_PATH)/rootdir/system/halium/etc/NetworkManager/conf.d/fix-ap-with-password.conf:system/halium/etc/NetworkManager/conf.d/fix-ap-with-password.conf \
     $(DEVICE_PATH)/rootdir/system/halium/etc/NetworkManager/conf.d/.halium-overlay-dir:system/halium/etc/NetworkManager/conf.d/.halium-overlay-dir \
-    $(DEVICE_PATH)/rootdir/system/halium/etc/init/bluetooth-touch-android.conf:system/halium/etc/init/bluetooth-touch-android.conf \
     $(DEVICE_PATH)/rootdir/system/halium/usr/share/bluetooth-touch/android.sh:system/halium/usr/share/bluetooth-touch/android.sh \
     $(DEVICE_PATH)/rootdir/system/halium/usr/share/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android \
     $(DEVICE_PATH)/rootdir/system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android \
-    $(DEVICE_PATH)/rootdir/system/halium/usr/share/upstart/sessions/usensord.conf:system/halium/usr/share/upstart/sessions/usensord.conf \
     $(DEVICE_PATH)/rootdir/system/halium/etc/deviceinfo/devices/halium.yaml:system/halium/etc/deviceinfo/devices/halium.yaml \
     $(DEVICE_PATH)/rootdir/system/halium/etc/fscrypt.conf:system/halium/etc/fscrypt.conf
 
